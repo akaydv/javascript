5858c2fc26594d2b4041f536a697ba88be0fad21
akaydv <ydvak9775@gmail.com>
2024-11-13 16:27:06 +0530

    hjkjj

---
+++ java/datatypes.js
@@ -0,0 +1,34 @@
+"use strict" // treat all js code as newer version---> it means to say that i am using newer version of js i.e all the syntax etc are newer
+//alert(3+3) ---->> it will throw error because  we are running in node js and not browser
+
+console.log(3+3) 
+console.log("akash")
+
+
+// https://tc39.es/ecma262/  -->>   reference
+// mdn web doc -->> reference
+
+let name="akash"
+let age=78
+let isLoggedIn=false
+//number ==> 2 to the power 53
+// bigint ==>> is number is very large then we use this bigint to store the value of a vraible number ex;facebook ,reddit etc
+// string ==>> " "
+// boolean=> true/false
+// null=> standalone value
+// undefined => means we have created variable but not have assigned value. 
+//symbol=>unique
+
+//object
+
+console.log(typeof age );
+console.log(typeof(age));
+console.log(typeof(isLoggedIn));
+console.log(typeof(name));
+console.log(typeof(null));  //type of an null is object
+console.log(typeof(undefined));// undefined 
+
+
+
+
+
---
+++ java/variables.js
@@ -0,0 +1,23 @@
+const accountId = 14556 //const value cannot be changed 
+let accountEmail = "aksdj23@gmail.com"
+var accountPassword = "14342"
+accountCity = "udaipur" // not best practice to write like this
+let accountState; // output will be undefined
+// accountID=355 ---> NOT POSSIBLE IT WILL THROW ERROR
+
+console.log(accountEmail);
+accountEmail="ydvnega@gmail.com"
+console.log(accountEmail);
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
+/*
+to declare const there is only one method
+but for variable there are two method i.e var and let 
+---- var is not a gooood practice to write in  code because of block scope and functional scope  
+
+*/
+console.log(accountState); 
+
+
+
+
