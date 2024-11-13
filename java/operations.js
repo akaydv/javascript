3b5e4419b9e615975645a431f8336bd7c9d92718
akaydv <ydvak9775@gmail.com>
2024-11-13 15:12:02 +0000

    operations

---
+++ java/operations.js
@@ -0,0 +1,65 @@
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+let str1="hello"
+let str2="akash"
+let str3=str1+str2
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1 + "2");
+// console.log("1"+ "2");
+// console.log(1+ 3 + "2" + "2");
+// console.log("1"+2+2);//if string is first then all is considered as string
+// console.log(1+2+"2");// if string is after number then number is added and then string
+
+// console.log((3+4)*5%3); //not used practically only in exams
+
+// console.log(+true); // it will give 1 as a output
+// console.log(true+);  //it will throw error
+
+// console.log(+""); // it will give 0 as output
+
+let num1,num2,num3,num4
+num1=num2=num3=num4=4+2
+// console.log(num1)
+
+// let gameCounter=100
+// gameCounter++;  //postfix increment
+// console.log(gameCounter);
+
+
+let gameCounter=100
+++gameCounter; //prefix increment
+// console.log(gameCounter);
+
+
+let a=5;
+console.log(a++);
+console.log(a);
+
+let b =6;
+console.log(++b);
+console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
