3046f2086592bd5279d05fab69bdb8018d93ab8b
akaydv <ydvak9775@gmail.com>
2024-11-13 17:08:31 +0530

    conversion of data types

---
+++ java/conversion.js
@@ -0,0 +1,87 @@
+let score="33"
+// console.log(typeof(score));
+
+let valueInNumber= Number(score)
+// console.log(typeof(valueInNumber))
+
+
+let marks= "33abc"
+// console.log(typeof(marks));
+let stringToNumber=Number(marks)
+// console.log(typeof stringToNumber);
+
+// console.log(stringToNumber);
+
+let result=null
+
+// console.log(result);
+// console.log(typeof result);
+
+let resultValue=Number(result)
+
+// console.log(resultValue);
+
+// console.log(typeof resultValue);
+
+
+// -------------------****************--------------------
+
+
+let akash= true
+// console.log(typeof(akash));
+
+let booleanToNumber=Number(akash)
+
+// console.log(booleanToNumber);
+// console.log(typeof booleanToNumber);
+
+
+// "33"=>> 33
+// "33abc"=>>NaN
+// true =>1,false=>0
+
+
+// let isLoggedIn=1
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+
+
+let isLoggedOut=""
+let booleanIsLoggedOut= Boolean(isLoggedOut)
+
+
+// console.log(typeof isLoggedOut);
+
+// console.log(typeof booleanIsLoggedOut);
+// console.log(booleanIsLoggedOut);
+
+// 1=>>true; 0=>false
+//""=>>false ,"akash" =>>true
+
+
+let a=5;
+let b=8;
+let sum=a+b;
+// console.log(sum);
+
+let someNumber =33;
+let stringNummber=String(someNumber);
+console.log(stringNummber);
+
+console.log(typeof stringNummber);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ java/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+console.log(typeof isLoggedOut);
+
+// console.log(typeof booleanIsLoggedOut);
+// console.log(booleanIsLoggedOut);
