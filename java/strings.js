112dfe6fa262e338b158d51e30be6fcf1844b480
akaydv <ydvak9775@gmail.com>
2024-11-14 13:48:45 +0000

    strings method

---
+++ java/strings.js
@@ -0,0 +1,43 @@
+// const name="akash"
+// const repoCount = 50
+
+// console.log(name + repoCount + " value"); //not a good way to concatinate
+
+// console.log(`hello my name is ${name} and my repocount is ${repoCount}`);
+
+// const gameName = new String("akASHYdv-hc")
+// console.log(gameName);
+// console.log(gameName[0]);//key-value pair
+// console.log(gameName.length);
+// console.log(gameName.__proto__);
+// console.log(gameName.toLowerCase());
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('S'));
+
+// const newString = gameName.substring(0,4); // it will exclude the character at 4 index element and return the value i.e=>>akaS as a output
+// it ignores -ve value and consider them as +ve and then give answer
+
+// console.log(newString);
+// const anotherString = gameName.slice(-8,4) //slice takes -ve value 
+
+
+
+
+
+
+// refer w3scool tutorail 
+
+const str = "The morning is upon us.";
+console.log(str.slice(4));
+//  console.log(str.slice(-1)); 
+// str.slice(-3, -1);
+// str.slice(0, -1); 
+console.log(str.slice(11,-7));
+// console.log(str.slice(4, -1));
+// console.log(str.slice(3, -1));
+// console.log(str.length);
+
+
+
+
